96d672dfcd05be1d86d553658d2762c7a2576385
zzdia <zzdia@BOON-SURFACE>
2016-09-02 00:13:23 -0500

    fixed Application Insights

--- DrumpfReport.Web/App_Start/FilterConfig.cs
+++ DrumpfReport.Web/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace DrumpfReport.Web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
